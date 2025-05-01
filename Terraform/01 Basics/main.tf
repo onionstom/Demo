eb58d05263f8b9b9ac2a09b9f6c20dce8a6223d9
onionstom <onionstom@yahoo.co.uk>
2025-05-01 15:28:40 +0100

    Main upload

---
+++ Terraform/01 Basics/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"    
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_instance" "test ubuntu instance" {
+    ami = "ami-04e7764922e1e3a57"   #   Amazon Linux Free Tier instance
+    instance_type = "t2.micro"
+  
+}
