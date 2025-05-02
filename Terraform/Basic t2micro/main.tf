7cc31f4258b6478d859bb6eb2efbd38f24fea948
onionstom <onionstom@yahoo.co.uk>
2025-05-02 11:04:14 +0100

    Change to name of instance

--- Terraform/Basic t2micro/main.tf
+++ Terraform/Basic t2micro/main.tf
@@ -18,7 +18,7 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-resource "aws_instance" "test ubuntu instance" {
+resource "aws_instance" "test_ubuntu_instance" {
     ami = "ami-04e7764922e1e3a57"   #   Amazon Linux Free Tier instance
     instance_type = "t2.micro"
 }
