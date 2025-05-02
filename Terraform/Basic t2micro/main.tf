5e566a831487331dc18c4ba667ff062b03e50d49
onionstom <onionstom@yahoo.co.uk>
2025-05-02 11:54:36 +0100

    reformatting creds

--- Terraform/Basic t2micro/main.tf
+++ Terraform/Basic t2micro/main.tf
@@ -15,9 +15,9 @@ terraform {
 }
 
 provider "aws" {
+  access_key = "AWS_ACCESS_KEY_ID"
+  secret_key = "AWS_SECRET_ACCESS_KEY"
   region = "eu-west-1"
-  access_key = "${AWS_ACCESS_KEY_ID}"
-  secret_key = "${AWS_SECRET_ACCESS_KEY}"
 }
 
 resource "aws_instance" "test_ubuntu_instance" {
