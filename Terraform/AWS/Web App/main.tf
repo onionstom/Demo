e1a9c6f5ea524cd16fc478d9d37667c7b914a436
onionstom <onionstom@yahoo.co.uk>
2025-05-03 20:39:53 +0100

    amending web app main. removing stale connection details

--- Terraform/AWS/Web App/main.tf
+++ Terraform/AWS/Web App/main.tf
@@ -1,19 +1,3 @@
-terraform {
-  backend "remote" {
-    organization = "TomOnionsTesting"
-
-    workspaces {
-      name = "Demo"
-    }
-    
-  }
-  required_providers {
-    aws = {
-        source = "hashicorp/aws"    
-    }
-  }
-}
-
 provider "aws" {
   region = "eu-west-1"
 }
