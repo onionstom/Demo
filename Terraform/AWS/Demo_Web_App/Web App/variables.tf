41be63f7b0dc77072e4315aeda4104e3b2d0360c
onionstom <onionstom@yahoo.co.uk>
2025-05-07 09:24:33 +0100

    change DB variables to sensitive

--- Terraform/AWS/Demo_Web_App/Web App/variables.tf
+++ Terraform/AWS/Demo_Web_App/Web App/variables.tf
@@ -1,6 +1,7 @@
 variable "DB_Name" {
   type        = string
   description = "The name of the database"
+  sensitive = true
 }
 variable "DB_Password" {
   type        = string
@@ -10,4 +11,5 @@ variable "DB_Password" {
 variable "DB_User" {
   type        = string
   description = "The nuser for the database"
+  sensitive = true
 }
