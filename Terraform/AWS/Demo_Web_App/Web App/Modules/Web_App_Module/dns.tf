4612694d687bb6e56fbff0015698ba64e4ec366a
onionstom <onionstom@yahoo.co.uk>
2025-05-13 21:34:29 +0100

    Vaiable Fixes

--- Terraform/AWS/Demo_Web_App/Web App/Modules/Web_App_Module/dns.tf
+++ Terraform/AWS/Demo_Web_App/Web App/Modules/Web_App_Module/dns.tf
@@ -13,7 +13,7 @@ data "aws_route53_zone" "selected" {
 
 # Set the record name, either the main domain or subdomain based on app_name variable
 locals {
-  dns_zone_id = var.create_dns_zone ? aws_route53_zone.selected[0].zone_id : data.aws_route53_zone.primary[0].zone_id
+  dns_zone_id = var.create_dns_zone ? aws_route53_zone.selected[0].zone_id : data.aws_route53_zone.selected[0].zone_id
   subdomain   = var.app_name == "main" ? "" : "${var.app_name}."
 
 }
@@ -21,7 +21,7 @@ locals {
 
 # Create the A record
 resource "aws_route53_record" "a_record" {
-  zone_id = data.aws_route53_zone.selected.zone_id
+  zone_id = locals.dns_zone_id
   name    = "${locals.subdomain}${var.domain}"
   type    = "A"
 
